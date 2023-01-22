2e570741eaf5508f9637e600ffe81ae648996de0
FrontendReactJS <allgloryforrobots@gmail.com>
2023-01-22 18:28:35 +0300

    part2

---
+++ commants.md
@@ -0,0 +1,3 @@
+tsc -init создать typescript.json (содержит нужные настройки в закомментированном виде)
+tsc - w
+tsc --watch hot reload ts
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// class Person {
+//     constructor(private name: string) {
+
+//     }
+// }
+
+// const max = new Person('Maxim')
+
+const button = document.querySelector("#btn");
+
+if (button) {
+    button.addEventListener("click", () => console.log(1))
+}
