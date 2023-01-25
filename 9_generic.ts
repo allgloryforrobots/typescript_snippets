b53304e64e816ca5c073a2f9393c6eafa2280603
user <frontendreactjs@gmail.com>
2023-01-25 17:44:25 +0300

    generics

--- 9_generic.ts
+++ 9_generic.ts
@@ -7,15 +7,67 @@ const promise = new Promise<string>( resolve => {
     }, 2000)
 })
 
-promise.then(
-    data => {
-        console.log(data.toUpperCase())
+promise.then( data => console.log(data.toUpperCase()) )
+
+const promise2: Promise<number> = new Promise( resolve => setTimeout(() => resolve(1), 2000))
+
+function mergeObjects<T extends object, R extends object>(a: T, b: R): T & R {
+    return Object.assign({}, a, b)
+}
+
+const merged = mergeObjects( { name: "Max" }, { age: 26 } )
+
+console.log(merged.age)
+
+const merged2 = mergeObjects( { model: "Ford" }, { year: 2010 } )
+
+interface ILength {
+    length: number
+}
+
+function withCount<T extends ILength>(value: T): { value: T, count: string } {
+    return {
+        value, 
+        count: `В этом объекте ${value.length} символов`
     }
-)
+}
+
+// keyof
+function getObjectvalue<T extends object, R extends keyof T>(obj: T, key: R) {
+    return obj[key]
+}
+
+const person = {
+    name: 'Max'
+}
+
+// getObjectvalue(person, 'job') // error
+// getObjectvalue(person, 'name')
 
 
-const promise2: Promise<number> = new Promise( resolve => {
-    setTimeout(() => {
-        resolve(1)
-    }, 2000)
-})
+// ================
+class Collection<T> {
+
+    // private _items: T[] = []
+
+    constructor(private _items: T[] = []) {
+
+    }
+
+    add(item: T) {
+        this._items.push(item)
+    }
+
+    remove(item: T) {
+        this._items = this._items.filter(i => i !== item)
+    }
+
+    get items(): T[] {
+        return this._items
+    }
+
+}
+
+const strings = new Collection<string>(["I", "A"])
+strings.add("!")
+strings.remove("!")
