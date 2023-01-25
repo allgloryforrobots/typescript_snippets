d01514cd10d7beec5fd0842782818f5c4b1f1f24
user <frontendreactjs@gmail.com>
2023-01-25 13:17:09 +0300

    'fix

--- 9_generic.ts
+++ 9_generic.ts
@@ -1,1 +1,21 @@
 const cars: string[] = ['Ford', 'Audi']
+const cars2: Array<string> = ['Ford', 'Audi']
+
+const promise = new Promise<string>( resolve => {
+    setTimeout(() => {
+        resolve("resolved")
+    }, 2000)
+})
+
+promise.then(
+    data => {
+        console.log(data.toUpperCase())
+    }
+)
+
+
+const promise2: Promise<number> = new Promise( resolve => {
+    setTimeout(() => {
+        resolve(1)
+    }, 2000)
+})
