a2cfe3a60e81857bd214ea29314c486f5aec1c18
user <frontendreactjs@gmail.com>
2023-02-01 08:58:08 +0300

    'end'

--- 11_namespaces.ts
+++ 11_namespaces.ts
@@ -1,5 +1,34 @@
-type FormType = 'inline' | 'block'
+// type FormType = 'inline' | 'block'
+// type FormState = 'active' | 'disabled'
 
-class MyForm {
-    private type :
+// interface FormInfo {
+//     type: FormType
+//     state: FormState
+// }
+
+/// <reference path="11_form-namespace.ts" />
+
+namespace IForm {
+    class MyForm {
+
+        private type: FormType = 'inline'
+        private state: FormState = 'active'
+    
+        constructor(public email: string) {
+    
+        }
+    
+        getInfo(): FormInfo {
+            return {
+                type: this.type,
+                state: this.state
+            }
+        }
+    
+    }
+    
+    const myForm = new MyForm('t@gmail.com')
+
+    
 }
+
